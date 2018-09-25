f7689e1716567605a86a7c7362e411df7f047096
liuzeyv123 <306012144@qq.com>
2018-09-25 16:49:30 +0800

    11

--- JavaScript/js2-桌游精灵/2-3/2-3.js
+++ JavaScript/js2-桌游精灵/2-3/2-3.js
@@ -8,34 +8,62 @@ window.onload=function(){
 	var div1=document.getElementById("div1");
 	var yes=document.getElementById("yes");
 	var no= document.getElementById("no");
-
+	var range=document.getElementById("range");
+	var left11=document.getElementById("left11");
+	var left22=document.getElementById("left22");
+//按钮跳转返回
 	img1.onclick=function(){
 		window.location.href="../2-1/2-1.html";
 	}
+ //玩家人数变化时,分配杀手平民的函数
+	function player(){
+				number.style.backgroundColor="#fff";
+				killer.innerHTML="&nbsp;";
+				human.innerHTML="&nbsp;";
+				if (reg.test(number.value)!=true) {
+					div1.style.display="block";
+				}
+				else{
+					killer.innerHTML=parseInt(number.value*1/4);
+					human.innerHTML=number.value - killer.innerHTML;
+					btn.onclick=function(){		
+						localStorage.setItem("killer",killer.innerHTML);
+						localStorage.setItem("human",human.innerHTML);
+						window.location.href="../2-4/2-4.html";
+				}
+			}
+		}
+
+
+//玩家人数输入框
 	number.onclick=function(){
 		number.style.backgroundColor="#dedede";
 	}
+	
 	number.onblur=function(){
-		number.style.backgroundColor="#fff";
-		killer.innerHTML="&nbsp;";
-		human.innerHTML="&nbsp;";
-		if (reg.test(number.value)!=true) {
-			div1.style.display="block";
-		}
-		else{
-			killer.innerHTML=parseInt(number.value*1/4);
-			human.innerHTML=number.value - killer.innerHTML;
-			btn.onclick=function(){		
-				localStorage.setItem("killer",killer.innerHTML);
-				localStorage.setItem("human",human.innerHTML);
-				window.location.href="../2-4/2-4.html";
-			}
-		}
+		player();
+		range.value=number.value;
 	}
+//弹窗=================
 	yes.onclick=function(){
 		div1.style.display="none";
 	}
 	no.onclick=function(){
 		div1.style.display="none";
+	}
+//滑动条=========
+	range.oninput=function(){
+		number.value=range.value;
+		player();
+	}
+	left11.onclick=function(){
+		range.value--;
+		number.value=range.value;
+		player();
+	}
+	left22.onclick=function(){
+		range.value++;
+		number.value=range.value;
+		player();
 	}
 }	
