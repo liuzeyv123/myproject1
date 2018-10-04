be3390eda60d7c27cbc6f1a728d162d444e25834
liuzeyv123 <306012144@qq.com>
2018-10-05 02:16:09 +0800

    11

--- JavaScript/js2-桌游精灵/2-6/2-6.js
+++ JavaScript/js2-桌游精灵/2-6/2-6.js
@@ -12,60 +12,149 @@ $(document).ready(function () {
 		window.location.href="../2-5/2-5.html";
 	})
 	//有限状态机
-var fsm = new StateMachine({
-	init: 'homicide',
-	transition:[
-			{name:'play_one',form:'start',to:'night'},
-			{name:'play_two',form:'night',to:'told'},
-			{name:'play_three',form:'told',to:'speakEnd'},
-			{name:'play_four',foem:'speakEnd',to:'voted'}
-	]
-});
+	var kill = $(".play_one");
+	var lastWorld = $(".play_two");
+	var speak = $(".play_three");
+	var vote = $(".play_four");
+	
+	var state = localStorage.state;
+	if ( state == null ){
+			state = 'alive';
+		}
+	if (state == 'voted' ){
+			state = 'alive';
+		}
+	fsm = new StateMachine({
+		init:state,
+		//状态转变
+		transition:[
+			{name:'kill',form:'alive',to:'dead'},
+			{name:'lastWorld',form:'dead',to:'spoke'},
+			{name:'speak',form:'spoke',to:'known'},
+			{name:'vote',form:'known',to:'voted'},
+		],
+		//各个状态时按键的颜色
+		methods:{
+			onDead:function(){
+				kill.css("background-color", "#000");
+			},
+			onSpoke:function(){
+				kill.css("background-color", "#000");
+				lastWorld.css("background-color", "#000");
+			},
+			onKnown:function(){
+				kill.css("background-color", "#000");
+				lastWorld.css("background-color", "#000");
+				speak.css("background-color", "#000");
+			},
+			onVoted:function(){
+				kill.css("background-color", "#000");
+				lastWorld.css("background-color", "#000");
+				speak.css("background-color", "#000");
+				vote.css("background-color", "#000");
+			}
+		}
 
-methods:{
-	onPlay_one:function(){
-		$(".play_one").css("backgroundColor","#2375af");
-		$(".sanjiao").eq(0).css("borderRightColor","#2375af");
+	})
+	//点击第一个杀人按钮时触发的事件
+	kill.click(function(){
+		if (fsm.state=="alive") {
+			fsm.kill();                                //改变当前状态;
+			//window.location.href ="../2-5/2-5.html";
+			localStorage.state=fsm.state;			   //存储改变后的状态;
+
+		}else{
+			alert("请按顺序操作");
+		}
+	})
+	//点击第二个遗言按钮时触发的事件
+	lastWorld.click(function(){
+		if (fsm.state=='dead'){
+			alert("请死者亮明身份并且发表遗言");
+			fsm.lastWorld();
+			localStorage.state=fsm.state;
+		}else{
+			alert("请按顺序操作");
+		}
 		
-	},
-	onPlay_two:function(){
-		$(".play_two").css("backgroundColor","#000");
-		$(".sanjiao").eq(1).css("borderRightColor","#000");
-		alert("请死者发表遗言");
-	},
-	onPlay_three:function(){
-		$(".play_three").css("backgroundColor","#000");
-		$(".sanjiao").eq(2).css("borderRightColor","#000");
-		alert("请玩家依次发言");
-	},
-	onPlay_four:function(){
-		$(".play_four").css("backgroundColor","#000");
-		$(".sanjiao").eq(3).css("borderRightColor","#000");
-		
-	}
-}
- switch (fsm.state){
-        case "night" :
-            fsm.onPlay_one();
-            aboutPlay_one();
-            break;
-        case "told":
-            fsm.onPlay_one();
-            fsm.onPlay_two();
-            break;
-        case "speakEnd":
-            fsm.onPlay_one();
-            fsm.onPlay_two();
-           	fsm.onPlay_three();
-            break;
-        case "voted":
-            fsm.onPlay_one();
-            fsm.onPlay_two();
-           	fsm.onPlay_three();
-           	fsm.onPlay_four();
-            aboutPlay_four();
-            aboutPlay_one();
-    }
+	})
+	//点击第三个讨论按钮时触发的事件
+	speak.click(function(){
+		if (fsm.state=='spock') {
+			alert("请按顺序依次发言");
+			fsm.speak();
+			localStorage.state=fsm.state;
+		}else{
+			alert("请按顺序操作");
+		}
+	})
+	//点击第四个投票按钮时触发的事件
+	vote.click(function(){
+		if (fsm.state=='konwn') {
+			fsm.vote();
+			//window.location.href="";
+			lacalStorage.state=fsm.state;
+		}else{
+			alert("请按顺序操作");
+		}
+	})
+})
+//以下皆为废案========暂存
+// var fsm = new StateMachine({
+// 	init:state,
+// 	transition:[
+// 			{name:'play_one',form:'start',to:'night'},
+// 			{name:'play_two',form:'night',to:'told'},
+// 			{name:'play_three',form:'told',to:'speakEnd'},
+// 			{name:'play_four',foem:'speakEnd',to:'voted'}
+// 	],
+
+
+// 	methods:{
+// 		onPlay_one:function(){
+// 			$(".play_one").css({backgroundColor:"#2375af"});
+// 			$(".sanjiao").eq(0).css({borderRightColor:"#2375af"});	
+// 		},
+// 		onPlay_two:function(){
+// 			$(".play_two").css({backgroundColor:"#000"});
+// 			$(".sanjiao").eq(1).css({borderRightColor:"#000"});
+// 			alert("请死者发表遗言");
+// 		},
+// 		onPlay_three:function(){
+// 			$(".play_three").css({backgroundColor:"#000"});
+// 			$(".sanjiao").eq(2).css({borderRightColor:"#000"});
+// 			alert("请玩家依次发言");
+// 		},
+// 		onPlay_four:function(){
+// 			$(".play_four").css({backgroundColor:"#000"});
+// 			$(".sanjiao").eq(3).css({borderRightColor:"#000"});	
+// 		}
+// 	}
+// });
+//  switch (fsm.state){
+//         case "night":
+//             fsm.onPlay_one();
+//             break;
+//         case "told":
+//             fsm.onPlay_one();
+//             fsm.onPlay_two();
+//             break;
+//         case "speakEnd":
+//             fsm.onPlay_one();
+//             fsm.onPlay_two();
+//            	fsm.onPlay_three();
+//             break;
+//         case "voted":
+//             fsm.onPlay_one();
+//             fsm.onPlay_two();
+//            	fsm.onPlay_three();
+//            	fsm.onPlay_four();    
+         
+// 	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	///
+
 // $('.play_one').click(function(){
 // 	fsm.killer();
 // })
--- JavaScript/js2-桌游精灵/2-7/2-7.js
+++ JavaScript/js2-桌游精灵/2-7/2-7.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var player=localStorage.getItem("player").split(",");
-
+	var dead="";
 	$(".img1").click(function(){
 		window.location.href="../2-3/2-3.html";
 	})
@@ -9,13 +9,19 @@ $(document).ready(function(){
 		$(".user").eq(i).show();
 		$(".user1").eq(i).html(player[i]);
 	}
-	
-	
 	$(".hover").hide();
 	$(".user").click(function(){
-		console.log($(this));
-		console.log($(this).index());
+		$(".hover").hide();
+		$(this).find(".hover").show();
+		var dead=$(this).index();
+		console.log(dead);
+		$("#start").click(function(){
+			if (player[dead]!=="杀手") {
+				console.log($(".user1").eq(dead));
+				$(".user1").eq(dead).css("background-color","#83b09a");
+			}else{
+				alert("你不能杀死队友");
+			}
+		})
 	})
-	console.log($(".user"));
-
 })
